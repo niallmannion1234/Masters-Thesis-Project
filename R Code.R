5defd2a3cb72e3986e37226f40d1678d10fd9f1b
niallmannion1234 <45274520+niallmannion1234@users.noreply.github.com>
2021-07-16 03:34:04 +0100

    Update R Code.R

--- R Code.R
+++ R Code.R
@@ -1,27 +1,3 @@
-# Install and load require packages
-install.packages(c("SuperLearner", "ipred", "adabag", "randomForest", "dplyr", "e1071", "nnet",
-                   "tidytext", "rtweet", "ggplot2", "tidyverse", "gridExtra", "tidyr", "plyr", 
-                   "randomForest", "kernlab", "neuralnet", "ggrepel", "translateR", "gtrendsR",
-                   "MASS", "DMwR", "C50", "fUnitRoots", "radarchart", "DT", "tsoutliers", "tseries",
-                   "MTS", "vars", "syuzhet", "forecastML", "twitteR", "wordcloud2", "plotly",
-                   "BBmisc", "grid", "ggplotify", "UBL", "reshape2", "ROAuth", "caret", "kernlab",
-                   "cowplot", "mice", "mlr", "ranger", "ROSE", "C50", "gmodels", "RTextTools",
-                   "Boruta", "tidyverse", "ROAuth", "gtrendsR", "plyr", "maps", "ggrepel", "gbm",
-                   "translateR", "tidyr", "tm", "wordcloud2", "qdap", "radarchart", "MASS", "shiny",
-                   "caret", "class", "cowplot", "ggmap", "forecast", "partykit", "party", "RTextTools",
-                   "flexdashboard", "textdata", "tsoutliers", "plotly", "Hmisc"))
-# Load librarieslibrary(tsoutliers)7
-loading <- c("SuperLearner", "ipred", "adabag", "randomForest", "e1071", "nnet", "tsoutliers",
-             "tidytext", "rtweet", "ggplot2", "tidyverse", "gridExtra", "tidyr", "plotly",
-             "randomForest", "kernlab", "neuralnet", "ggrepel", "translateR", "gtrendsR",
-             "MASS", "DMwR", "C50", "fUnitRoots", "radarchart", "DT", "tsoutliers", "tseries",
-             "MTS", "vars", "syuzhet", "forecastML", "twitteR", "wordcloud2", "plotly",
-             "BBmisc", "grid", "ggplotify", "UBL", "reshape2", "ROAuth", "caret", "kernlab",
-             "cowplot", "mice", "mlr", "ranger", "ROSE", "C50", "gmodels", "textdata",
-             "Boruta", "tidyverse", "ROAuth", "gtrendsR", "dplyr", "maps", "ggrepel", "gbm",
-             "translateR", "tidyr", "tm", "wordcloud2", "qdap", "radarchart", "MASS", "shiny",
-             "caret", "class", "cowplot", "ggmap", "forecast", "partykit", "party", "flexdashboard")
-lapply(loading, require, character.only = TRUE)
 setwd("D:\\1 Masters\\Dataset\\trainingandtestdata")
 
 # Scrape and analyst data from Google trends
