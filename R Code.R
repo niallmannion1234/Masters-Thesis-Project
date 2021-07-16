fdaa9847b2cd4365ef04acb33709a1ca3f0a75f1
niallmannion1234 <45274520+niallmannion1234@users.noreply.github.com>
2021-07-16 03:51:59 +0100

    Update R Code.R

--- R Code.R
+++ R Code.R
@@ -3,26 +3,6 @@ setwd("D:\\AD")
 write.csv(test, file= "test_data.csv")
 write.csv(trains,"training_data.csv")
 
-# 2. Bagging
-set.seed(322) 
-untuned_bag <- bagging( 
-  formula = Vaccination_Status ~ ., 
-  data = trains) 
-untuned_bagging_pred <- predict(untuned_bag, test) 
-untuned_bagging_target <- as.factor(untuned_bagging_pred$class)
-set.seed(322) 
-bagging_model <- bagging( 
-  formula = Vaccination_Status ~ ., 
-  data = trains, 
-  nbagg = 100)
-tuned_bagging_pred <- predict(bagging_model, test) 
-tuned_bagging_target <- as.factor(tuned_bagging_pred$class) 
-
-confusionMatrix(untuned_bagging_target,
-                test$Vaccination_Status)
-confusionMatrix(tuned_bagging_target,
-                test$Vaccination_Status) 
-
 # 3. KNN
 # Make the factor levels suitable for models such as knn. This keeps the variables as factors but changes
 # the factor level names to simple numerical values that for models
